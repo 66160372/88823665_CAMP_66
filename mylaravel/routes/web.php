bfb90894321f0bfbf9d33b8228de0e8c1f55f101
66160372 <66160372@go.buu.ac.th>
2025-02-05 02:31:59 +0700

    Update_2

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -5,12 +5,16 @@ use App\Http\Controllers\MyController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
 
 Route::get('/login',
     [LoginController::class, 'index']);
 
 Route::get('/register',
     [RegisterController::class, 'index']);
+
+Route::post('/register',
+    [RegisterController::class, 'create']);
 
 Route::get('/home',
 [HomeController::class, 'index']);
@@ -31,3 +35,8 @@ Route::get('/mycontroller/{id?}',
 
 Route::post('/mycontroller/{id?}',
 [MyController::class,'myfunction']);
+
+Route::get('/users',[UserController::class,'index']);
+Route::get('/user/{id}',[UserController::class,'edit']);
+Route::put('/user',[UserController::class,'edit_action']);
+Route::delete('/users', [UserController::class, 'delete']);
